6f18804a374948f5a5088d43e7d57974c95adde6
Niyaspa <niyaspa8342@gmail.com>
2025-03-04 15:17:45 +0530

    Add files via upload

---
+++ 30_MOHAMMED NIYAS P A/05.Cpu/Cpu.java
@@ -0,0 +1,32 @@
+package MODULE1;
+
+import java.util.Scanner;
+
+public class Cpu {
+
+	double price;
+	
+	class Processor
+	{
+		double cores;
+		String manufacturer;
+		double getCache(double c)
+		{
+
+			return c;
+		}
+	}
+	
+	static class RAM
+	{
+		Scanner sc = new Scanner(System.in);
+		double memory;
+		String manufacturer;
+		double getClockSpeed(double sp)
+		{
+
+			return sp;
+		}
+	}
+
+}
