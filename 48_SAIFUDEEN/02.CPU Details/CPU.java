b5976c0ecc8f12e8a92315471eb90d93d486b072
Saifudeen369 <saifudheen232003@gmail.com>
2025-03-04 14:20:03 +0530

    Create CPU.java

---
+++ 48_SAIFUDEEN/02.CPU Details/CPU.java
@@ -0,0 +1,19 @@
+package MODULE1;
+
+public class CPU {
+	double price;
+	class Processor{
+		double cores;
+		String manufacturer;
+		double getCache() {
+			
+		}
+	}
+	static class RAM{
+		double memory;
+		String manufacturer;
+		double getClockSpeed() {
+			
+		}
+	}
+}	
