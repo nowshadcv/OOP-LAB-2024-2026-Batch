43237ee1df4318b64e25ec4da40cea5efb4bec66
naadhi-web <naadhifaisal@gmail.com>
2025-03-04 14:42:11 +0530

    Create CPU.java

---
+++ 36_NAADHI RAHMAN P/05. Cpu Details/CPU.java
@@ -0,0 +1,18 @@
+package MODULE1;
+
+public class CPU {
+	double price;
+	class Processor {
+		double cores ;
+		String manufacturer;
+		double getCache (double f) {
+			return f;}}
+	static class RAM{
+		double memory;
+		String manufacturer;
+		double getClockspeed(double r) {
+			return r;}
+		}
+	}
+		
+	
