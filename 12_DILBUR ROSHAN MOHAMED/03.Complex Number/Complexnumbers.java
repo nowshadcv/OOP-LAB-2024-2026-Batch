3c8125781423a18539b178d6ec90475599703e96
Dilbur roshan mohamed <148055236+dilburroshanmohamed@users.noreply.github.com>
2025-03-04 14:32:12 +0530

    Add files via upload

---
+++ 12_DILBUR ROSHAN MOHAMED/03.Complex Number/Complexnumbers.java
@@ -0,0 +1,41 @@
+package MODULE1;
+
+import java.util.Scanner;
+
+public class Complexnumbers {
+    double real, img;
+
+    Complexnumbers(double r, double i) {
+        real = r;
+        img = i;
+    }
+
+    public static Complexnumbers sum(Complexnumbers c1, Complexnumbers c2) {
+        return new Complexnumbers(c1.real + c2.real, c1.img + c2.img);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the real part of first complex number:");
+        double real1 = scanner.nextDouble();
+        System.out.println("Enter the imaginary part of first complex number:");
+        double img1 = scanner.nextDouble();
+        
+        System.out.println("Enter the real part of second complex number:");
+        double real2 = scanner.nextDouble();
+        System.out.println("Enter the imaginary part of second complex number:");
+        double img2 = scanner.nextDouble();
+
+        Complexnumbers c1 = new Complexnumbers(real1, img1);
+        Complexnumbers c2 = new Complexnumbers(real2, img2);
+
+        Complexnumbers temp = sum(c1, c2);
+
+        System.out.println("Sum is " + temp.real + " + " + temp.img + "i");
+
+        scanner.close();
+    }
+}
+
+
