409525056d6eacffcf446a3086f690f8e07ffdec
anjuE9431 <anjuanilkumar292@gmail.com>
2025-03-04 15:05:56 +0530

    Update Cpu.java

--- 06_ANJU E/05.Cpu/Cpu.java
+++ 06_ANJU E/05.Cpu/Cpu.java
@@ -1,26 +1,30 @@
-package MODULE1;
+package module_1;
+public class CPU{
+	double price;
+	class Processor{
+		double cores;
+		String Manufacturer;
+		double Cache;
+		public Processor(double cache)
+		{
+			Cache = cache;
+		}
+		double getCache() {
+			return Cache;
+		}
+//		double getCache(double a) {return a}
+	}
+	static class Ram{
+		double memory,clockSpeed;
+		String Manufacurer;
+		Ram(double cs)
+		{
+			clockSpeed = cs;
+		}
+		double getClockSpeed() {
+			return clockSpeed;
+		}
+//		double getClockSpeed(double b) {return b}
+	}
 
-public class Cpu {
-    double price;
-
-    
-    class processor {
-        double cores;
-        String manufacturer;
-        
-        double getCache() {
-            return 4.3;
-        }
-    }
-
-   
-    static class RAM {
-        double memory;
-        String manufacturer;
-
-        double getClockspeed() {
-            return 5.5;
-        }
-    }
 }
-
