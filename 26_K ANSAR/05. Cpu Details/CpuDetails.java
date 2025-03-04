8bd8b06a9a34f44c31d4f7b90f43f573a1998ee6
ansarmca <ansarkp300@gmail.com>
2025-03-04 14:43:39 +0530

    Create CpuDetails.java

---
+++ 26_K ANSAR/05. Cpu Details/CpuDetails.java
@@ -0,0 +1,46 @@
+package module_1;
+
+import java.util.Scanner;
+
+class CPU {
+    double price;
+
+    class Processor {
+        double cores;
+        String manufacturer;
+        double cache;
+
+        double getCache() {
+            return cache;
+        }
+    }
+
+    static class RAM {
+        double memory;
+        String manufacturer;
+        double clockSpeed;
+
+        double getClockSpeed() {
+            return clockSpeed;
+        }
+    }
+}
+
+public class CpuDetails {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        CPU cpu = new CPU();
+        CPU.Processor processor = cpu.new Processor();
+        System.out.print("Enter cache value for the processor: ");
+        processor.cache = scanner.nextDouble();
+        scanner.nextLine(); 
+        CPU.RAM ram = new CPU.RAM();
+        System.out.print("Enter clock speed for RAM: ");
+        ram.clockSpeed = scanner.nextDouble();
+        System.out.println("\nProcesor Details:");
+        System.out.println("Processor Cache = " + processor.getCache());
+        System.out.println("RAM Clock Speed = " + ram.getClockSpeed());
+
+
+    }
+}
