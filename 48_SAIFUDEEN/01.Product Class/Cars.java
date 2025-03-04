a3294e4d3eba20901976bfa1bdf48022525c0381
Saifudeen369 <saifudheen232003@gmail.com>
2025-03-04 14:01:35 +0530

    Update Cars.java

--- 48_SAIFUDEEN/01.Product Class/Cars.java
+++ 48_SAIFUDEEN/01.Product Class/Cars.java
@@ -8,8 +8,8 @@ public class cars {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter details for Product 1:");
-        System.out.print("Enter Product Code: ");
+        System.out.println(" details for Product 1:");
+        System.out.print(" Product Code: ");
         String pcode1 = scanner.nextLine();
         System.out.print("Enter Product Name: ");
         String pname1 = scanner.nextLine();
@@ -19,8 +19,8 @@ public class cars {
 
         Product p1 = new Product(pname1, pcode1, price1);
 
-        System.out.println("\nEnter details for Product 2:");
-        System.out.print("Enter Product Code: ");
+        System.out.println("\n details for Product 2:");
+        System.out.print(" Product Code: ");
         String pcode2 = scanner.nextLine();
         System.out.print("Enter Product Name: ");
         String pname2 = scanner.nextLine();
@@ -30,8 +30,8 @@ public class cars {
 
         Product p2 = new Product(pname2, pcode2, price2);
 
-        System.out.println("\nEnter details for Product 3:");
-        System.out.print("Enter Product Code: ");
+        System.out.println("\n details for Product 3:");
+        System.out.print(" Product Code: ");
         String pcode3 = scanner.nextLine();
         System.out.print("Enter Product Name: ");
         String pname3 = scanner.nextLine();
@@ -40,11 +40,11 @@ public class cars {
 
         Product p3 = new Product(pname3, pcode3, price3);
 
-        System.out.println("\nDisplaying Product 1:");
+        System.out.println("\n Product 1:");
         p1.display();
-        System.out.println("\nDisplaying Product 2:");
+        System.out.println("\n Product 2:");
         p2.display();
-        System.out.println("\nDisplaying Product 3:");
+        System.out.println("\n Product 3:");
         p3.display();
 
         Product p = p3.getPrice() < (p1.getPrice() < p2.getPrice() ? p1.getPrice() : p2.getPrice()) ? p3 : (p1.getPrice() < p2.getPrice() ? p1 : p2);
