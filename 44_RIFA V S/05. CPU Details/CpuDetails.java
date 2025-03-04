f20f3183edb9841d67e0aa08d0b478fbfeab0e2f
rifavs <rifavs40@gmail.com>
2025-03-04 01:19:51 -0800

    Update and rename CpuDetails.java to CpuDetails.java

--- 44_RIFA V S/05. CPU Details/CpuDetails.java
+++ 44_RIFA V S/05. CPU Details/CpuDetails.java
@@ -1,36 +1,3 @@
-package module_1;
-
-import java.util.Scanner;
-
-class CPU 
-{
-    double price;
-
-    class Processor 
-    {
-        double cores;
-        String manufacturer;
-        double cache;
-
-        double getCache() 
-        {
-            return cache;
-        }
-    }
-
-    static class RAM 
-    {
-        double memory;
-        String manufacturer;
-        double clockSpeed;
-
-        double getClockSpeed() 
-        {
-            return clockSpeed;
-        }
-    }
-}
-
 public class CpuDetails 
 {
     public static void main(String[] args) 
