41e5d48bae71b283ddd9b738c6f89dffbba3d4c5
shimnasherin052 <shimnasherin052@gmail.com>
2025-03-04 15:13:12 +0530

    Create CPUDetails.java

---
+++ 53_SHIMNA SHERIN P T/CPU Details/CPUDetails.java
@@ -0,0 +1,21 @@
+package MODULE1;
+
+import java.util.Scanner;
+
+
+public class CPUDetails {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner (System.in);
+		System.out.println("Enter processor cache: ");
+		double p = s.nextDouble();
+		System.out.println("Enter RAM Clock speed: ");
+		double r = s.nextDouble();
+		CPU cpu =new CPU();
+		CPU.Processor processor=cpu.new Processor();
+		CPU.RAM ram=new CPU.RAM();
+		System.out.println("Processor Cache = "+processor.getCache(p));
+		System.out.println("RAM Clock speed= "+ ram.getClockSpeed(r));
+	}
+
+}          
