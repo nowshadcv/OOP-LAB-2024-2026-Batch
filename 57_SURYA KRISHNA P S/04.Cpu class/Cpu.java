92e1d37c875121e993f12725861211c2628b7729
Surya-33 <suryakrishnafeb132004@gmail.com>
2025-03-04 15:05:40 +0530

    Update Cpu.java

--- 57_SURYA KRISHNA P S/04.Cpu class/Cpu.java
+++ 57_SURYA KRISHNA P S/04.Cpu class/Cpu.java
@@ -1,1 +1,20 @@
+package MODULE1;
+
+public class CPU {
+	double price;
+	class Processor{
+		double cores;
+		String manufacturer;
+		double getcache() {
+			return 4.3;
+		}
+	}
+	static class RAM{
+		double memory;
+		String manufacturer;
+		double getclockspeed() {
+			return 5.5;
+		}
+	}
+}
 
