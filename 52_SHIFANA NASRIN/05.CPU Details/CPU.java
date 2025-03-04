a65528f5b5b41514eb66c667cdd57fc37482e01f
Shifananasrin <shifananasrin941@gmail.com>
2025-03-04 15:00:02 +0530

    Create CPU.java

---
+++ 52_SHIFANA NASRIN/05.CPU Details/CPU.java
@@ -0,0 +1,22 @@
+package MODULE_1;
+
+import java.util.Scanner;
+
+class CPU {
+	double price;
+	class Processor{
+		double cores;
+		String manufacturer;
+		double getCache(double f) {
+			return f;
+		}
+	}
+
+	static class RAM {
+		double memory;
+		String manufacturer;
+		double getClockSpeed(double r) {
+			return r;
+		}
+	}
+}
