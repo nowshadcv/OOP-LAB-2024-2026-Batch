4a778e67447c3df98a4f2e2b0b27183c252d4f32
Nafeesa0 <148351447+Nafeesa0@users.noreply.github.com>
2025-03-04 15:17:44 +0530

    Add files via upload

---
+++ 37_NAFEESA K T/05.CPU/CPU.java
@@ -0,0 +1,29 @@
+package MODULE_1;
+
+public class CPU{
+	double price;
+	class Processor{
+		double cores;
+		String Manufacturer;
+		double Cache;
+		public Processor(double cache)
+		{
+			Cache = cache;
+		}
+		double getCache() {
+			return Cache;
+		}
+	}
+	static class Ram{
+		double memory,clockSpeed;
+		String Manufacurer;
+		Ram(double cs)
+		{
+			clockSpeed = cs;
+		}
+		double getClockSpeed() {
+			return clockSpeed;
+		}
+	}
+
+}
---
+++ 37_NAFEESA K T/05.CPU/CPUDetails.java
@@ -0,0 +1,19 @@
+package MODULE_1;
+
+import java.util.Scanner;
+
+public class CPUDetails {
+    public static void main(String[] args) {
+        CPU cpu = new CPU();
+        Scanner s = new Scanner(System.in);
+        System.out.println("What's cache of your processor");
+        double cache = s.nextDouble();
+        System.out.println("Ram ClockSpeed?");
+        double clockSpeed = s.nextDouble();
+        CPU.Processor p = cpu.new Processor(cache);
+        CPU.Ram ram = new CPU.Ram(clockSpeed);
+        s.close();
+        System.out.println("Processor Cache  " + p.getCache() + " MB");
+        System.out.println("Ram Clock Speed  " + ram.getClockSpeed() + " GHz");
+    }
+}
