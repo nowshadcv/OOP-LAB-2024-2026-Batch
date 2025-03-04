e011d773e59ae7c2437093529e61989135f316d2
Basim-Shebin <basimshebin@gmail.com>
2025-03-04 14:48:35 +0530

    Create CPU.java

---
+++ 10_BASIM SHEBIN K P/05. CPU Class/CPU.java
@@ -0,0 +1,32 @@
+package MODULE_1;
+
+import java.util.Scanner;
+
+public class CPU {
+
+	double price;
+	
+	class Processor
+	{
+		double cores;
+		String manufacturer;
+		double getCache(double c)
+		{
+
+			return c;
+		}
+	}
+	
+	static class RAM
+	{
+		Scanner sc = new Scanner(System.in);
+		double memory;
+		String manufacturer;
+		double getClockSpeed(double sp)
+		{
+
+			return sp;
+		}
+	}
+
+}
