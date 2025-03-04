9dd1ec9f6673556065b5608127767b02af2ec267
adhiAdthya <adhyxhhh@gmail.com>
2025-03-04 14:45:15 +0530

    Create SymmetricMatrix.java

---
+++ 02_ADITHYA DAS M P/04.Symmetric Matrix/SymmetricMatrix.java
@@ -0,0 +1,43 @@
+package Module1;
+import java.util.Scanner;
+
+public class SymmetricMatrix {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter number of rows: ");
+		int rows = s.nextInt();
+		System.out.println("Enter number of columns: ");
+		int cols = s.nextInt();
+		int matrix[][] = new int[rows][cols];
+		System.out.println("Enter the elements of the matrix: ");
+		for(int i=0;i<rows;i++) {
+			for(int j=0;j<cols;j++)
+				matrix[i][j] = s.nextInt();
+		}
+		s.close();
+		System.out.println("The given matrix : ");
+		for(int i=0;i<rows;i++) {
+			for(int j=0;j<cols;j++)
+				System.out.print(matrix[i][j] + "\t");
+			System.out.println();
+		}
+		if(rows != cols)
+			System.out.println("The given matrix is not a square matrix");
+		else {
+			boolean symmetric = true;
+			for(int i=0;i<rows;i++)
+				for(int j=0;j<cols;j++)
+					if(matrix[i][j] != matrix[j][i]) {
+						symmetric = false;
+						break;
+					}
+			if(symmetric) {
+				System.out.println("The given matrix is symmetric");
+			}
+			else {
+				System.out.println("the given matrix is not symmetric");
+			}
+		}
+	}
+}
