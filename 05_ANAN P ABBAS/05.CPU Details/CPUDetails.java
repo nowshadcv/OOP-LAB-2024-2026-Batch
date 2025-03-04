c3d87fe5be8c6fececadc2bd127d358dea914852
ananpabbas <ananpa2323@gmail.com>
2025-03-04 15:09:10 +0530

    Create CPUDetails.java

---
+++ 05_ANAN P ABBAS/05.CPU Details/CPUDetails.java
@@ -0,0 +1,22 @@
+package module_1;
+
+import java.util.Scanner;
+
+public class CPUDetails{
+	public static void main(String[] args) {
+		CPU cpu = new CPU();
+		Scanner s = new Scanner(System.in);
+		System.out.println("What's cache of your processor");
+		double cache = s.nextDouble();
+		System.out.println("Ram ClockSpeed?");
+		double clockSpeed = s.nextDouble();
+		CPU.Processor p = cpu.new Processor(cache);
+		CPU.Ram ram = new CPU.Ram(clockSpeed);
+		s.close();
+		System.out.println("Processor Cache  "+p.getCache()+" MB");
+		System.out.println("Ram Clock Speed  "+ram.getClockSpeed()+" GHz");
+//		System.out.println(p.getCache(cache));
+//		System.out.println(ram.getClockSpeed(clockSpeed));
+	}
+	
+}
