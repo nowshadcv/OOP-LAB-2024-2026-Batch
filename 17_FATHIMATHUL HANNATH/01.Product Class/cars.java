f7de43322a98444e94ff51b6b936fa4ada90f233
Dilbur roshan mohamed <148055236+dilburroshanmohamed@users.noreply.github.com>
2025-03-04 14:50:49 +0530

    Add files via upload

---
+++ 17_FATHIMATHUL HANNATH/01.Product Class/cars.java
@@ -0,0 +1,28 @@
+package MODULE1;
+
+public class cars {
+	public static void main(String[] args) {
+		product p1 = new product();
+		p1.pcode = "car123";
+		p1.pname = "benz";
+		p1.price = 10000;
+		System.out.println("displaying p1 : ");
+		p1.display();
+		
+		
+		product p2 = new product("jagur", "cars426" ,25000);
+		System.out.println("displaying p2 : ");
+		p2.display();
+		
+		
+		product p3 = new product("maruthi", "cars800" ,50000);
+		System.out.println("displaying p3 : ");
+		p3.display();
+		
+		product p = p3.getPrice()<(p1.price<p2.price?p1.price:p2.price)?p3: (p1.price<p2.price?p1:p2);//
+		System.out.println("\ndisplaying product with lowest price : ");	
+		p.display();
+		
+	}
+
+}
