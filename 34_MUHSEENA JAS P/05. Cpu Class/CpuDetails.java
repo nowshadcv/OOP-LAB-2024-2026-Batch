92dc394306e7e2b6507a44963d9f471fbe1fecd0
Muhseenajas <muhseenajas2003@gmail.com>
2025-03-04 15:08:49 +0530

    Create CpuDetails.java

---
+++ 34_MUHSEENA JAS P/05. Cpu Class/CpuDetails.java
@@ -0,0 +1,26 @@
+public class CpuDetails {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        CPU cpu = new CPU();
+
+        CPU.Processor processor = cpu.new Processor();
+
+        System.out.print("Enter cache value for the processor: ");
+        processor.cache = scanner.nextDouble();
+        scanner.nextLine(); 
+
+        CPU.RAM ram = new CPU.RAM();
+
+        System.out.print("Enter clock speed for RAM: ");
+        ram.clockSpeed = scanner.nextDouble();
+        
+        System.out.println("\nProcessor Details:");
+        System.out.println("Processor Cache = " + processor.getCache() + " MB");
+       
+        System.out.println("RAM Clock Speed = " + ram.getClockSpeed() + " GHz");
+
+
+    }
+}
