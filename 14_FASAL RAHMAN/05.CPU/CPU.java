1711e394e99cefcd8a0a3e2ae30a7e3554456dc5
fasal rahman <fasalrahmankalappattil@gmail.com>
2025-03-04 14:17:23 +0530

    Create CPU.java

---
+++ 14_FASAL RAHMAN/05.CPU/CPU.java
@@ -0,0 +1,25 @@
+package MODULE_1;
+
+public class CPU {
+	double price;
+	class Processor{
+		double cores,Cache;
+		String manufacturer;
+		Processor(double cache){
+			Cache = cache;
+		}
+		double getCach() {
+			return Cache;
+		}
+	}
+	static class RAM{
+		double memory,Clockspeed;
+		String manufacturer;
+		RAM(double clockspeed){
+			Clockspeed = clockspeed;
+		}
+		double getClockSpeed() {
+			return Clockspeed;
+		}
+	}
+}
