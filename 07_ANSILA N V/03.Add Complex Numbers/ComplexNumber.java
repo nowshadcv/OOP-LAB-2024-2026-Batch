db5bc0d90a70ffd9e9ff5652c9b6b5f464e99909
ansilasharaf <ansilanachiveettil@gmail.com>
2025-03-04 15:04:01 +0530

    Add files via upload

---
+++ 07_ANSILA N V/03.Add Complex Numbers/ComplexNumber.java
@@ -0,0 +1,43 @@
+package MODULE1;
+
+import java.util.Scanner;
+
+public class ComplexNumber {
+	double real,img;
+	ComplexNumber(double r,double i) {
+		real = r ;
+		img = i ;
+	}
+	public static ComplexNumber sum(ComplexNumber c1,ComplexNumber c2) {
+		ComplexNumber temp = new ComplexNumber(0,0);
+		temp.real = c1.real + c2.real;
+		temp.img = c1.img + c2.img;
+		return temp;
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the first real number:");
+		double real1 = scanner.nextDouble();
+		System.out.println("Enter the first imaginary number:");
+		double img1 = scanner.nextDouble();
+			
+		System.out.println("Enter the second real number:");
+		double real2 = scanner.nextDouble();
+		System.out.println("Enter the second imaginary number:");
+		double img2 = scanner.nextDouble();
+			
+			
+			
+			
+		ComplexNumber c1 = new ComplexNumber(real1,img1);
+		ComplexNumber c2 = new ComplexNumber(real2,img2);
+		ComplexNumber temp =sum(c1,c2);
+		System.out.printf("Sum is :"+temp.real+"+"+temp.img +"i");// TODO Auto-generated method stub
+
+		scanner.close();
+	}
+
+}
