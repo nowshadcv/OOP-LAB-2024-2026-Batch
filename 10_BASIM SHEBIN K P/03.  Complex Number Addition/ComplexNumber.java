fd29fcdbe4602b165ea8c0ba39cd57bd7ba4a867
Basim-Shebin <basimshebin@gmail.com>
2025-03-04 14:35:02 +0530

    Create ComplexNumber.java

---
+++ 10_BASIM SHEBIN K P/03.  Complex Number Addition/ComplexNumber.java
@@ -0,0 +1,43 @@
+package MODULE_1;
+import java.util.Scanner;
+public class ComplexNumber {
+
+	double real, img;
+	ComplexNumber(double r, double i) 
+	{
+		real = r; img = i;
+	}
+	
+	public static ComplexNumber sum(ComplexNumber c1, ComplexNumber c2)
+	{
+		ComplexNumber temp = new ComplexNumber(0,0);
+		temp.real = c1.real + c2.real;
+		temp.img = c1.img + c2.img;
+		return temp;
+	}
+
+	public static void main(String[] args) 
+	{
+		Scanner s = new Scanner(System.in);
+		double num1,num2;
+		System.out.println("Enter real part of complex number 1:");
+		num1 = s.nextDouble();
+		System.out.println("Enter imaginary part of complex number 1:");
+		num2 = s.nextDouble();
+		
+		ComplexNumber c1 = new ComplexNumber(num1,num2);
+		
+		System.out.println("Enter real part of complex number 2:");
+		num1 = s.nextDouble();
+		System.out.println("Enter imaginary part of complex number 2:");
+		num2 = s.nextDouble();
+		
+		ComplexNumber c2 = new ComplexNumber(num1,num2);
+		
+		ComplexNumber temp = sum(c1,c2);
+		
+		System.out.printf("Sum is:"+temp.real+"+"+temp.img+"i");
+		s.close();
+	}
+
+}
