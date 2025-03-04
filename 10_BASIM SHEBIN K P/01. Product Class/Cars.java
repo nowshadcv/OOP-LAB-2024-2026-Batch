f129693dc7386ab20c98bd3211b91d2a0bad46bb
Basim-Shebin <basimshebin@gmail.com>
2025-03-04 14:37:52 +0530

    Update and rename cars.java to Cars.java

---
+++ 10_BASIM SHEBIN K P/01. Product Class/Cars.java
@@ -0,0 +1,39 @@
+package MODULE_1;
+import java.util.Scanner;
+
+public class Cars {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        int objs, price;
+        String name, code;
+        int lowpr = 2147483647;
+        Product lowestPricedProduct = null;
+        
+        System.out.println("Enter the number of objects:");
+        objs = s.nextInt();
+
+        for (int i = 0; i < objs; i++) {
+            s.nextLine();
+            System.out.println("Enter the car product code:");
+            code = s.nextLine();
+            System.out.println("Enter the car name:");
+            name = s.nextLine();
+            System.out.println("Enter the car price:");
+            price = s.nextInt();
+            Product p = new Product(code, name, price);
+            p.display();
+
+            if (price < lowpr) {
+                lowpr = price;
+                lowestPricedProduct = p;
+            }
+        }
+        
+        if (lowestPricedProduct != null) {
+            System.out.println("\nDisplaying product with lowest value:");
+            lowestPricedProduct.display();
+        }
+    }
+}
