00594abc7293e3d8880c29b634bf49a8aab52c06
SETHULAKSHMI K <sethulakshm.i2221@gmail.com>
2025-03-04 14:55:36 +0530

    Add files via upload

---
+++ 50_SETHULAKSHMI K/03.  Complex Number/ComplexNumber.java
@@ -0,0 +1,35 @@
+package MODULE;
+
+import java.util.Scanner;
+
+public class ComplexNumber {
+   double real,img;
+   ComplexNumber (double r,double i){
+	   real =r; img=i;}
+   public static ComplexNumber sum(ComplexNumber c1,ComplexNumber c2) {
+	  ComplexNumber temp=new ComplexNumber(0,0);
+	  temp.real=c1.real+c2.real;
+	  temp.img=c1.img+c2.img;
+	  return temp;}
+   public static void main(String args[]) {
+	   Scanner sc = new Scanner(System.in);
+	   System.out.println("enter 1st complex number :");
+	   double r1 = sc.nextDouble();
+	   System.out.println("enter imaginary part :");
+	   double i1 = sc.nextDouble();
+	   System.out.println("enter 2nd complex number :");
+	   double r2 = sc.nextDouble();
+	   System.out.println("enter imaginary part :");
+	   double i2 = sc.nextDouble();
+	   sc.close();
+	   ComplexNumber c1 = new ComplexNumber(r1,i1);
+	   ComplexNumber c2 = new ComplexNumber(r2,i2);
+	   ComplexNumber temp = sum(c1,c2);
+	   System.out.printf("Sum is : "+ temp.real + "+" + temp.img +"i");
+   }
+	   
+	   
+   
+	  
+}
+
