12406e7d5219e179210c8f6d4cbf8d0d81e8dd38
Sneha <115629125+Snehasathyaa@users.noreply.github.com>
2025-03-04 14:32:11 +0530

    Update MatrixAdd.java

--- 54_SNEHA K P/02. Matrix Addition/MatrixAdd.java
+++ 54_SNEHA K P/02. Matrix Addition/MatrixAdd.java
@@ -1,48 +1,57 @@
 package MODULE_1;
-
 import java.util.Scanner;
+public class MatrixAdd {
 
-public class cars {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		
+		int p,q,m,n;
+		Scanner s=new Scanner(System.in);
+		System.out.print("Enter number of rows in Matrix A: ");
+		p=s.nextInt();
+		System.out.print("Enter number of column in Matrix A: ");
+		q=s.nextInt();
+		System.out.print("Enter number of rows in Matrix B: ");
+		m=s.nextInt();
+		System.out.print("Enter number of column in Matrix B: ");
+		n=s.nextInt();
+	    if(p==m && q==n) {
+	    	int a[][]=new int[p][q];
+	    	int b[][]=new int[m][n];
+	    	System.out.print("Enter the elements of Matrix A: ");
+			for(int i=0;i<p;i++)
+				for(int j=0;j<q;j++)
+					a[i][j]=s.nextInt();
+			System.out.print("Enter the elements of Matrix B: ");
+			for(int i=0;i<m;i++)
+				for(int j=0;j<n;j++)
+					b[i][j]=s.nextInt();
+			System.out.println("Matrix A: ");
+			for(int i=0;i<p;i++)
+				for(int j=0;j<q;j++)
+					System.out.print(a[i][j]+" ");
+			System.out.println();
+			System.out.println("Matrix B: ");
+			for(int i=0;i<m;i++)
+				for(int j=0;j<n;j++)
+					System.out.print(b[i][j]+" ");
+			System.out.println();
+			System.out.println("The sum Matrix: ");
+			for(int i=0;i<m;i++) 
+			
+				for(int j=0;j<n;j++)
+					System.out.print((a[i][j]+b[i][j])+" ");
+			System.out.println();
+			}
+			else {
+				System.out.println("These Matrices cannot be added  ");
+			}
+			
+			
+			
+			
+			
+	    }
 
-        System.out.print("Enter the number of products: ");
-        int n = scanner.nextInt();
-        scanner.nextLine(); 
-        
-        Product[] products = new Product[n];
-        
-        for (int i = 0; i < n; i++) {
-            System.out.println("\n\nEnter details for Product " + (i + 1) + ":");
-            System.out.print("Enter Product Code: ");
-            String pcode = scanner.nextLine();
-            System.out.print("Enter Product Name: ");
-            String pname = scanner.nextLine();
-            System.out.print("Enter Product Price: ");
-            int price = scanner.nextInt();
-            scanner.nextLine();  
+	}
 
-           
-            products[i] = new Product(pname, pcode, price);
 
-           
-            System.out.println("Displaying Product " + (i + 1) + ":");
-            products[i].display();
-        }
-
-       
-        Product lowestPriceProduct = products[0]; 
-        for (int i = 1; i < n; i++) {
-            if (products[i].getPrice() < lowestPriceProduct.getPrice()) {
-                lowestPriceProduct = products[i];
-            }
-        }
-
-      
-        System.out.println("\nDisplaying Product with Lowest Price  :");
-        lowestPriceProduct.display();
-
-        
-        scanner.close();
-    }
-}
