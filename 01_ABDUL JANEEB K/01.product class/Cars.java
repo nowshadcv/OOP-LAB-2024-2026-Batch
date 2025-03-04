3930c757317bf3683a188802791763bbc862e4bc
abduljaneeb <janeebkonami@gmail.com>
2025-03-04 20:40:46 +1200

    Update Cars.java

--- 01_ABDUL JANEEB K/01.product class/Cars.java
+++ 01_ABDUL JANEEB K/01.product class/Cars.java
@@ -1,40 +1,56 @@
 package MODULE1;
 import java.util.Scanner;
-class Cars {
+
+public class Cars {
 
 	public static void main(String[] args) {
-		int price;
-		String pname,pcode;
-		Scanner S=new Scanner(System.in);
-		Product p1=new Product();
-		System.out.print("Enter the pcode of first object: ");
-		p1.pcode=S.next();
-		System.out.print("Enter the pname of first object: ");
-		p1.pname=S.next();
-		System.out.print("Enter the price of first object: ");
-		p1.price=S.nextInt();
-		Product p2=new Product();
-		System.out.print("Enter the pcode of Second object: ");
-		p2.pcode=S.next();
-		System.out.print("Enter the pname of Second object: ");
-		p2.pname=S.next();
-		System.out.print("Enter the price of Second object: ");
-		p2.price=S.nextInt();
-		Product p3=new Product();
-		System.out.print("Enter the pcode of Third object: ");
-		p3.pcode=S.next();
-		System.out.print("Enter the pname of Third object: ");
-		p3.pname=S.next();
-		System.out.print("Enter the price of Third object: ");
-		p3.price=S.nextInt();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter details for Product 1:");
+        System.out.print("Enter Product Code: ");
+        String pcode1 = scanner.nextLine();
+        System.out.print("Enter Product Name: ");
+        String pname1 = scanner.nextLine();
+        System.out.print("Enter Product Price: ");
+        int price1 = scanner.nextInt();
+        scanner.nextLine();
+		
+        Product p1 = new Product(pname1, pcode1, price1);	
+		
+		System.out.println("Enter details for Product 2:");
+        System.out.print("Enter Product Code: ");
+        String pcode2 = scanner.nextLine();
+        System.out.print("Enter Product Name: ");
+        String pname2 = scanner.nextLine();
+        System.out.print("Enter Product Price: ");
+        int price2 = scanner.nextInt();
+        scanner.nextLine();
+		
+		Product p2=new Product(pname2,pcode2,price2);
+		
+		
+		System.out.println("Enter details for Product 3:");
+        System.out.print("Enter Product Code: ");
+        String pcode3 = scanner.nextLine();
+        System.out.print("Enter Product Name: ");
+        String pname3 = scanner.nextLine();
+        System.out.print("Enter Product Price: ");
+        int price3 = scanner.nextInt();
+        scanner.nextLine();
+		
+		Product p3 = new Product (pname3,pcode3,price3);
+		
 		System.out.println("Displaying p1:");
 		p1.display();
-		System.out.println("Displaying p2: ");
+		
+	
+		System.out.println("Dislaying p2: ");
 		p2.display();
-		System.out.println("Displaying p3:");
-		p3.display();
-		Product p=p3.getPrice()<(p1.price<p2.price?p1.price:p2.price)?p3:(p1.price<p2.price?p1:p2);
-		System.out.println("Displaying product with lowest price");
+		
+		System.out.println("Dislaying p3: ");
+		p2.display();
+		
+		Product p=p3.getprice()<(p1.price<p2.price?p1.price:p2.price)?p3:(p1.price<p2.price?p1:p2);
+		System.out.println("\nDisplaying produt with lowest price:");
 		p.display();
 		
 
