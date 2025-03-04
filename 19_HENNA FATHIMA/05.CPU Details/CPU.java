49d62a6b2e39e34f8bcc4bc3716e34fbff7fee38
henn-a <147914416+henn-a@users.noreply.github.com>
2025-03-04 15:10:31 +0530

    Add files via upload

---
+++ 19_HENNA FATHIMA/05.CPU Details/CPU.java
@@ -0,0 +1,30 @@
+package MODULE1;
+public class CPU{
+	double price;
+	class Processor{
+		double cores;
+		String Manufacturer;
+		double Cache;
+		public Processor(double cache)
+		{
+			Cache = cache;
+		}
+		double getCache() {
+			return Cache;
+		}
+//		double getCache(double a) {return a}
+	}
+	static class Ram{
+		double memory,clockSpeed;
+		String Manufacurer;
+		Ram(double cs)
+		{
+			clockSpeed = cs;
+		}
+		double getClockSpeed() {
+			return clockSpeed;
+		}
+//		double getClockSpeed(double b) {return b}
+	}
+
+}
