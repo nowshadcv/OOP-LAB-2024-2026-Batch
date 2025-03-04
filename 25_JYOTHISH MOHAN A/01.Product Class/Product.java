e9f91517742ca551ac69fe0d264faaa031dabd28
jyothishmohan <156563567+jyothishmohan@users.noreply.github.com>
2025-03-04 14:05:11 +0530

    Add files via upload

---
+++ 25_JYOTHISH MOHAN A/01.Product Class/Product.java
@@ -0,0 +1,30 @@
+package MODULE1;
+
+public class Product {
+	String pname,pcode;
+	int price;
+	public Product(){}
+	public Product(String pname, String pcode, int price) {
+		this.pname=pname;
+		this.pcode=pcode;
+		this.price=price;}
+	
+	public void setpname(String pname) {
+		this.pname=pname;}
+	public String getPcode() {
+		return pcode;}
+	public String getPname() {
+		return pname;}
+	public void setpcode(String pcode) {
+		this.pcode=pcode;}
+	public int getprice() {
+		return price;}
+	public void setprice(int price) {
+		this.price=price;}
+	public void display() {
+		System.out.println("pcode: "+this.pcode);
+		System.out.println("pname: "+this.pname);
+		System.out.println("price: "+this.price+"\n");
+		
+	}
+}	
