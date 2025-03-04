68a7124c20f426ab20b9a8752407118540d38613
YUSRA <yusraash44@gmail.com>
2025-03-04 14:57:17 +0530

    Add files via upload

--- 60_YUSRA K V/03.  Complex Numbers/ComplexNumber.java
+++ 60_YUSRA K V/03.  Complex Numbers/ComplexNumber.java
@@ -1,19 +1,33 @@
 package MODULE1;
-
-public class ComplexNumber {
+import java.util.Scanner;
+public class ComplexNumber 
+{
 	double real,img;
-	ComplexNumber(double r,double i) {
+	ComplexNumber(double r,double i) 
+	{
 		real=r;img=i;}
-	public static ComplexNumber sum(ComplexNumber c1,ComplexNumber c2) {
+	public static ComplexNumber sum(ComplexNumber c1,ComplexNumber c2)
+	{
 		ComplexNumber temp=new ComplexNumber(0,0);
 		temp.real=c1.real+c2.real;
 		temp.img=c1.img+c2.img;
-		return temp;}
+		return temp;
+		}
 	
-	public static void main(String args[]) {
-		ComplexNumber c1=new ComplexNumber(5.5,4);
-		ComplexNumber c2=new ComplexNumber(1.2,3.5);
-		ComplexNumber temp=sum (c1,c2);
+	public static void main(String args[])
+	{
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter real part of first complexnumber:");
+		double real1=s.nextDouble();
+		System.out.println("Enter image part of first complexnumber:");
+		double img1=s.nextDouble();
+		ComplexNumber c1=new ComplexNumber(real1,img1);
+		System.out.println("Enter real part of second complexnumber:");
+		double real2=s.nextDouble();
+		System.out.println("Enter image part of second complexnumber:");
+		double img2=s.nextDouble();
+		ComplexNumber c2=new ComplexNumber(real2,img2);
+		ComplexNumber temp=sum(c1,c2);
 		System.out.printf("sum is:"+temp.real+"+"+temp.img+"i");
 	}
 }
