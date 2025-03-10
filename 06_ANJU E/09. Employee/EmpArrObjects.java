294d6f95dd0ceca38202f5db38967d5c710c93f5
anjuE9431 <anjuanilkumar292@gmail.com>
2025-03-10 11:10:48 +0530

    Update EmpArrObjects.java

--- 06_ANJU E/09. Employee/EmpArrObjects.java
+++ 06_ANJU E/09. Employee/EmpArrObjects.java
@@ -1,24 +1,4 @@
-package MODULE1;
-//package MODULE1;
 
-//public class Employee {
-//	int eNo,eSalary;
-///	String eName;
-
-	//public Employee() {
-		// TODO Auto-generated constructor stub
-	//}
-	//public Employee(int no,int sal,String name)
-	//{
-		//eNo=no;
-	//	eSalary=sal;
-		/*eName=name;
-		public void showdata()
-		{
-			System.out.print("empid="+eNo+""+"name="+eName+""+"salary="+eSalary);
-			System.out.println();
-		}
-//	}*/
 	import java.util.Scanner;
 	public class EmpArrObjects{
 		
