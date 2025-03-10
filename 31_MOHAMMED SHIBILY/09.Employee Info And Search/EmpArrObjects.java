67d0a6fac1392136960d998ff24cf342df2d7293
Mohammed-shibily <shibilychalambattbackup@gmail.com>
2025-03-10 11:05:45 +0530

    EmpArrObjects.java

---
+++ 31_MOHAMMED SHIBILY/09.Employee Info And Search/EmpArrObjects.java
@@ -0,0 +1,41 @@
+package MODULE9;
+import java.util.Scanner;
+public class EmpArrObjects{
+	public static void main (String[] args) {
+		System.out.println("Enter number of employees :");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println("Enter Employee details one by one...");
+		Employee employees[] = new Employee[n];
+		Scanner scemp = new Scanner (System.in);
+		int eid,esal;
+		String ename;
+		for (int i=0; i<n; i++) {
+			System.out.println("Enter "+i+" employee details...");
+			System.out.println("Enter employee id (integer) :");
+			eid = scemp.nextInt();
+			System.out.println("Enter employee name (String) :");
+			String name = scemp.next();
+			ename = new String(name);
+			System.out.println("Enter employee salary (integer) :");
+			esal = scemp.nextInt();
+			Employee emp = new Employee(eid,esal,ename);
+			employees[i] = emp;}
+		System.out.println("Employees are:\n");
+		for (Employee y: employees)
+			y.showData();
+		System.out.println("Enter employee number to search :");
+		int semp = sc.nextInt();
+		boolean found = false;
+		for (Employee e: employees) {
+			if (semp == e.eNo) {
+				found = true;
+				System.out.println("Employee found.");
+				e.showData();
+				break;}}
+		if(!found)
+			System.out.println("Employee not found...");
+		scemp.close();
+		sc.close();
+		}
+}
