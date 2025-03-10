c6e63eaac68fde4f63d14775bc4f5c199f8b5127
Saran-sk52 <saransk1249@gmail.com>
2025-03-10 10:44:11 +0530

    Create Employee.java

---
+++ 49_SARAN S KRISHNAN/09. Employee Info And Search/Employee.java
@@ -0,0 +1,15 @@
+package MODULE_2;
+
+public class Employee {
+	int eNo,eSalary;
+	String eName;
+	public Employee() {}
+	public Employee (int no, int sal, String name) {
+		eNo = no;
+		eSalary = sal;
+		eName = name;}
+	public void showData() {
+		System.out.print("Empld = "+eNo+" "+"Name = "+eName+" 2"+"Salary = "+eSalary);
+		System.out.println();
+	}
+}
