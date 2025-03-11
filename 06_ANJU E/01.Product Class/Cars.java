ca53ce82c41dfe39c3d2f30e739de01fd11f5973
anjuE9431 <anjuanilkumar292@gmail.com>
2025-03-11 14:54:47 +0530

    Update Cars.java

--- 06_ANJU E/01.Product Class/Cars.java
+++ 06_ANJU E/01.Product Class/Cars.java
@@ -1,28 +1,65 @@
 package MODULE1;
 
-public class CARS
-{
-	
-		 public static void main(String[] args)
-		 {
-			 PRODUCT p1=new PRODUCT();
-			 p1.pcode="car123";
-			 p1.pname="benz";
-			 p1.price=1000;
-			 System.out.println("display p1:");
-			 p1.display();
-			 PRODUCT p2=new PRODUCT("JAGUR","CAR456",25000);
-			 System.out.println("display p2:");
-			 p2.display();
-			 PRODUCT p3=new PRODUCT("maruthi","car800",50000);
-			 PRODUCT p=p3.getprice()<(p1.price<p2.price?p1.price:p2.price)?p3:(p1.price<p2.price?p1:p2);
-			 System.out.println("\n   displaying product with lowest price:");
-			 p.display();
-		 
-					 
-			 
-		 }
-	}
+import java.util.Scanner;
 
+class Product {
+    String pname;
+    String pcode;
+    int price;
 
+    public Product(String pname, String pcode, int price) {
+        this.pname = pname;
+        this.pcode = pcode;
+        this.price = price;
+    }
 
+    public Product() {}
+    public void display() {
+        System.out.println("Product Code: " + pcode);
+        System.out.println("Product Name: " + pname);
+        System.out.println("Product Price: " + price);
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
+
+public class Cars {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("How many products would you like to enter? ");
+        int numProducts = scanner.nextInt();
+        scanner.nextLine(); 
+        Product[] products = new Product[numProducts];
+        for (int i = 0; i < numProducts; i++) {
+            System.out.println("\nEnter details for Product " + (i + 1) + ":");
+            System.out.print("Enter Product Code: ");
+            String pcode = scanner.nextLine();
+            System.out.print("Enter Product Name: ");
+            String pname = scanner.nextLine();
+            System.out.print("Enter Product Price: ");
+            int price = scanner.nextInt();
+            scanner.nextLine();
+            products[i] = new Product(pname, pcode, price);
+        }
+
+        for (int i = 0; i < numProducts; i++) {
+            System.out.println("\nDisplaying Product " + (i + 1) + ":");
+            products[i].display();
+        }
+
+        Product cheapestProduct = products[0];
+        for (int i = 1; i < numProducts; i++) {
+            if (products[i].getPrice() < cheapestProduct.getPrice()) {
+                cheapestProduct = products[i];
+            }
+        }
+        System.out.println("\nDisplaying the product with the lowest price:");
+        cheapestProduct.display();
+
+        scanner.close();
+    }
+}
