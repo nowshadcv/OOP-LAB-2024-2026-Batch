90e8cb925300956ab4d7136848f691698c86add9
jyothishmohan <156563567+jyothishmohan@users.noreply.github.com>
2025-03-11 15:08:09 +0530

    Update MatrixAddition.java

--- 25_JYOTHISH MOHAN A/02. Matrix Addition/MatrixAddition.java
+++ 25_JYOTHISH MOHAN A/02. Matrix Addition/MatrixAddition.java
@@ -1,57 +1,103 @@
 package MODULE1;
+
 import java.util.Scanner;
 
 public class MatrixAddition {
-
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-		
 		int p,q,m,n;
-		Scanner s = new Scanner (System.in);
-		System.out.print("Enter number of rows in matrix A : ");
-		p=s.nextInt();
-		System.out.print("Enter number of columns in matrix A : ");
-		q=s.nextInt();
-		System.out.print("Enter number of rows in Matrix B : ");
-		m=s.nextInt();
-		System.out.print("Enter number of columns in Matrix B : ");
-		n=s.nextInt();
-		
-		if(p==m && q==n) {
-			
-			int a[][] = new int[p][p];
+		Scanner s = new Scanner(System.in);
+		System.out.print("Enter rows of Matrix A ");
+		p = s.nextInt();
+		System.out.print("Enter columns of Matrix A ");
+		q = s.nextInt();
+		System.out.print("Enter rows of Matrix B ");
+		m = s.nextInt();
+		System.out.print("Enter columns of Matrix B ");
+		n = s.nextInt();
+		if(p == m && q == n)
+		{
+			int a[][] = new int[p][q];
 			int b[][] = new int[m][n];
-			
-			System.out.println("Enter the elements of Mtrix A : ");
-			for (int i=0; i<p; i++)
-				for(int j=0; j<p; j++)
+			System.out.println("Enter elements of Matrix A");
+			for(int i = 0; i < p; i++)
+				for(int j = 0; j < q; j++)
 					a[i][j] = s.nextInt();
+			System.out.println("Enter elements of Matrix B");
+			for(int i = 0; i < m; i++)
+				for(int j = 0; j < n; j++)
+					b[i][j] = s.nextInt();
 			
-			System.out.println("Enter the elements of matrix B : ");
-			for(int i=0; i<m; i++)
-				for(int j=0; j<n; j++)
-					b[i][j]=s.nextInt();
-			
-			
-			System.out.println("Matrix A : ");
-			for(int i=0; i<p; i++) {
-				for(int j=0; j<q; j++)
-					System.out.print(a[i][j] + " ");
-				System.out.println();}
-			System.out.println("Matrix B : ");
-			for(int i=0; i<m; i++) {
-				for(int j=0;j<n; j++)
-					System.out.print(b[i][j] + " ");
-				System.out.println();}
-			System.out.println("The sum Matrix : ");
-			for(int i=0; i<m; i++) {
-				for(int j=0; j<n; j++)
-					System.out.print((a[i][j]+b[i][j])+" ");
-				System.out.println();}
-		}
-		else {
-				System.out.println("Matrices cannot be added");}
+			System.out.println("Matrix A");
+			for(int i = 0; i < p; i++) {
+				for(int j = 0; j < q; j++)
+					System.out.print(a[i][j] + "  ");
+				System.out.println();
+			}
+			System.out.println("Matrix B");
+			for(int i = 0; i < m; i++) {
+				for(int j = 0; j < n; j++)
+					System.out.print(b[i][j] + "  ");
+				System.out.println();
+			}
+			int choice = 0;
+			while(choice != 6) {
+				System.out.println("Choose one from the MENU below:");
+				System.out.println(" Addition\n2. Subtraction\n3. Division\n4. Multiplication\n5. Transpose\n6. Exit");
 				
-
-	}
-
-}
+				choice = s.nextInt();
+			switch(choice) {
+				case 1:
+					for(int i = 0; i < m; i++)
+					{
+						for(int j = 0; j < n; j++)
+							System.out.print((a[i][j] + b[i][j]) + "  ");
+						System.out.println();
+					}
+					break;
+				case 2:
+					for(int i = 0; i < m; i++)
+					{
+						for(int j = 0; j < n; j++)
+							System.out.print((a[i][j] - b[i][j]) + "  ");
+						System.out.println();
+					}
+					break;
+				case 3:
+					for(int i = 0; i < m; i++)
+					{
+						for(int j = 0; j < n; j++)
+							System.out.print((a[i][j] / b[i][j]) + "   ");
+						System.out.println();
+						System.out.println();
+					}
+					break;
+				case 4:
+					for(int i = 0; i < m; i++)
+					{
+						for(int j = 0; j < n; j++)
+							System.out.print((a[i][j] * b[i][j]) + "  ");
+						System.out.println();
+					}
+					break;
+				case 5:
+					System.out.println("Transpose of Matrix A");
+					for(int i = 0; i < m; i++)
+					{
+						for(int j = 0; j < n; j++)
+							System.out.print(a[j][i] + "  ");						
+						System.out.println();
+					}
+					System.out.println("Transpose of Matrix B");
+					for(int i = 0; i < m; i++)
+					{
+						for(int j = 0; j < n; j++)
+							System.out.print(b[j][i] + "  ");						
+						System.out.println();
+					}
+					break;
+				case 6:
+					System.exit(1);}}}
+		else {
+			System.out.println("Matrices should be square.");
+		}}}
