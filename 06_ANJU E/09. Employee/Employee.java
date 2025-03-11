15edd4571118d3a8c11d7e21ad5a852a5c74db90
anjuE9431 <anjuanilkumar292@gmail.com>
2025-03-11 15:29:57 +0530

    Update Employee.java

--- 06_ANJU E/09. Employee/Employee.java
+++ 06_ANJU E/09. Employee/Employee.java
@@ -17,23 +17,4 @@ public class Employee {
 			System.out.print("empid="+eNo+""+"name="+eName+""+"salary="+eSalary);
 			System.out.println();
 		}
-	}package MODULE1;
-
-public class Employee {
-	int eNo,eSalary;
-	String eName;
-
-	public Employee() {
-		// TODO Auto-generated constructor stub
-	}
-	public Employee(int no,int sal,String name)
-	{
-		eNo=no;
-		eSalary=sal;
-		eName=name;}
-		public void showData()
-		{
-			System.out.print("empid="+eNo+""+"name="+eName+""+"salary="+eSalary);
-			System.out.println();
-		}
 	}
