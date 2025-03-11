1a8b74f3a1d5f57b09e5108a9d430e9c40d2f9b3
Aminanazar1 <aminanazar2511@gmail.com>
2025-03-11 15:05:00 +0530

    Add files via upload

--- 04_AMINA PM/09.Employee/Employee.java
+++ 04_AMINA PM/09.Employee/Employee.java
@@ -11,22 +11,23 @@ public class Employee {
 	public void showData() {
 		System.out.println("EmpId ="+eNo+" "+"Name="+eName+"" + "Salary="+ eSalary);
 		System.out.println(); }
-	public class EmpArrObjects {
-		public void main(String[] args) {
+	public static class EmpArrObjects {
+		public static void main(String[] args) {
 			System.out.println("enter number of employees:");
 			Scanner sc= new Scanner(System.in);
 			int n = sc.nextInt();
-			System.out.println("enter employee details one by one..");
+			System.out.println("enter employee details one by one:");
 			Employee employee[] =new Employee[n];
 			Scanner scemp = new Scanner(System.in);
 			int eid,esal;
 			String enam;
 			for(int i=0; i<n;i++) {
-				System.out.println("enter"+1+"employee details..");
+				System.out.println("enter " +1+ " employee details:");
 				System.out.println("enter employee id(integer):");
 				eid=scemp.nextInt();
+				scemp.nextLine();
 				System.out.println("enter employee name (String):");
-				String nam=scemp.next();
+				String nam=scemp.nextLine();
 				enam=new String (nam);
 				System.out.println("enter employee salary(integer):");
 				esal=scemp.nextInt();
