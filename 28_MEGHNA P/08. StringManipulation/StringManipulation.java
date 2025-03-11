67fd1a732095e6bb616ac87ed9a83147ffb9a0b8
Meghna2004p <meghnamohanan2004@gmail.com>
2025-03-11 14:50:05 +0530

    Add files via upload

---
+++ 28_MEGHNA P/08. StringManipulation/StringManipulation.java
@@ -0,0 +1,41 @@
+package MODULE1;
+
+public class StringManipulation {
+	public static void main(String[] args) {
+		char arrSample[]= {'R','O','S','E'};
+		String strSample1= new String( arrSample );
+		System.out.println(strSample1 + "\t");
+		System.out.println("Length of"+strSample1+"is:"+strSample1.length());
+		byte ascii[]= {65,66,67,68,69,70};
+		String strSample2=new String(ascii);
+		System.out.println(strSample2+"\t");
+		String strSample3=(strSample1.concat(strSample2));
+		System.out.println(strSample3+"\t");
+		String strSample4=strSample1+strSample2;
+		System.out.println(strSample4+"\t");
+		System.out.println("3rd char of "+strSample4+" is:"+ strSample4.charAt(2));
+		char buf[]=new char[3];
+		strSample4.getChars(2, 5, buf, 0);
+		System.out.println("Extracted chars:"+strSample4+""+ new String(buf));
+		String strSample="Rockstar";
+		System.out.println(strSample.compareTo("ROCKSTAR")+"/t");
+		System.out.println(strSample.compareToIgnoreCase("ROCKSTAR")+"\t");
+		System.out.println(strSample.equals("ROCKSTAR")+"\t");
+		System.out.println(strSample.equalsIgnoreCase("ROCKSTAR")+"\t");
+		System.out.println("Rockstar starts with'Ro':"+strSample.startsWith("Ro"));
+		System.out.println("Rockstar ends with'star':"+strSample.endsWith("star"));
+		System.out.println("Rockstar contains 'tar':"+strSample.contains("tar"));
+		System.out.println("index of't'in Rockstar:"+strSample.indexOf("t"));
+		System.out.println("index of'star'in Rockstar:"+strSample.indexOf("star"));
+		System.out.println("All caps-Rockstar:"+strSample.toUpperCase());
+		System.out.println("All small=rock star:"+strSample.toLowerCase());
+		System.out.println("Replace star with et:"+strSample.replace("star","et"));
+		String dval="6";
+		String ival="6";
+		System.out.println("Double value of dval is:"+Double.valueOf(dval));
+		System.out.println("nteger value of ival is:"+Integer.valueOf(ival));
+		double a=Double.valueOf(dval);
+		int b=Integer.valueOf(ival);
+		System.out.println(a+b);
+}
+}
