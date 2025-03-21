76959c0d76617bfce37134f498618ed3adf16ee5
eswardas <eswardaskn@gmail.com>
2025-03-21 14:11:40 +0530

    Update OverLoad.java

--- 13_ESWARDAS_K_NAIR/10.OverLoad/OverLoad.java
+++ 13_ESWARDAS_K_NAIR/10.OverLoad/OverLoad.java
@@ -1,4 +1,6 @@
 package MODULE_2;
+import java.util.Scanner;
+
 
 public class OverLoad 
 {
@@ -6,9 +8,21 @@ public class OverLoad
 	public static void main(String[] args) 
 	{
 		OverLoadDemo ob = new OverLoadDemo();
-		ob.area(5);
-		ob.area(11,12);
-		ob.area(2.5);
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println("Enter the length of a side of the square:");
+		float side = s.nextFloat();
+		ob.area(side);
+		
+		System.out.println("Enter the length of the rectangle:");
+		float length = s.nextFloat();
+		System.out.println("Enter the width of the rectangle:");
+		float width = s.nextFloat();
+		ob.area(length,width);
+		
+		System.out.println("Enter the radius of the circle:");
+		double radius = s.nextDouble();
+		ob.area(radius);
 	}
 
 }
