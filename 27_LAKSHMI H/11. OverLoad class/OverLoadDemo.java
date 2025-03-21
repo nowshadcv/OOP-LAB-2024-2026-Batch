72b4a577e0b181c4e9b595f0c87e7b2c900832f0
Lakshmi H <100887312+Lakshmiha@users.noreply.github.com>
2025-03-21 14:35:37 +0530

    Add files via upload

--- 27_LAKSHMI H/11. OverLoad class/OverLoadDemo.java
+++ 27_LAKSHMI H/11. OverLoad class/OverLoadDemo.java
@@ -1,7 +1,7 @@
 package MODULE2;
-
+import java.util.Scanner;
 public class OverLoadDemo {
-	void are(float x) {
+	void area(float x) {
 		System.out.println("The area of square is:"+Math.pow(x, 2)+" sq units");
 	}
 	void area(float x,float y) {
@@ -15,9 +15,22 @@ public class OverLoadDemo {
 
 class OverLoad{
 	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
 		OverLoadDemo ob=new OverLoadDemo();
-		ob.area(5);
-		ob.area(11,12);
-		ob.area(2.5);
+		
+		System.out.println("Enter the side of the square:");
+		float squareSide=sc.nextFloat();
+		ob.area(squareSide);
+		
+		System.out.println("Enter the length of rectangle:");
+		float length=sc.nextFloat();
+		System.out.println("Enter the breadth of rectangle:");
+		float breadth=sc.nextFloat();
+		ob.area(length,breadth);
+		
+		System.out.println("Enter the radius:");
+		double radius=sc.nextDouble();
+		ob.area(radius);
+		sc.close();
 	}
 }
