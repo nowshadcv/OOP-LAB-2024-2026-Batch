c526f4ef78fcd57e586ef34960da8a54342ef1aa
ananpabbas <ananpa2323@gmail.com>
2025-03-21 14:22:51 +0500

    Create Overload.java

---
+++ 05_ANAN P ABBAS/11. Area Using  Over Load/Overload.java
@@ -0,0 +1,14 @@
+package MODULE2;
+
+public class Overload {
+	public static void main(String args[]) {
+		OverLoadDemo ob =new OverLoadDemo();
+		ob.area(5);
+		ob.area(11,12);
+		ob.area(2.5);
+		
+	}
+
+	}
+
+
