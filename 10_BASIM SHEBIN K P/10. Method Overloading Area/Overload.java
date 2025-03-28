f1041f00a14ff1f2082995fc02946c20d2b974b1
Basim-Shebin <basimshebin@gmail.com>
2025-03-28 14:29:18 +0530

    Create Overload.java

---
+++ 10_BASIM SHEBIN K P/10. Method Overloading Area/Overload.java
@@ -0,0 +1,23 @@
+package MODULE2;
+
+import java.util.Scanner;
+
+public class Overload {
+	public static void main(String[] args) {
+		
+		OverloadDemo obj = new OverloadDemo();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter Size of Square: ");
+		int size = scanner.nextInt();
+		System.out.println("Enter Length of Rectangle");
+		int l = scanner.nextInt();
+		System.out.println("Enter width of Rectangle");
+		int w = scanner.nextInt();
+		System.out.println("Enter Side of Triangle");
+		double side = scanner.nextDouble();
+		obj.area(size);
+		obj.area(w, l);
+		obj.area(side);
+	}
+
+}
