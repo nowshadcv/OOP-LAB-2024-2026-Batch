5073babe63c21ec788c5289409a8c63a1a87b658
Basim-Shebin <basimshebin@gmail.com>
2025-03-28 14:27:25 +0530

    Create Employee.java

---
+++ 10_BASIM SHEBIN K P/09. Employee Info And Search/Employee.java
@@ -0,0 +1,15 @@
+package MODULE2;
+
+public class Employee {
+	int eNo, eSalary;
+	String eName;
+	public Employee(int no, int sal, String name) {
+		this.eNo = no;
+		this.eSalary = sal;
+		this.eName = name;
+	}
+	public void showData(){
+		System.out.println("EmpId: "+eNo+"\nName: "+eName+"\nSalary: "+eSalary);
+		System.out.println();
+	}
+}
