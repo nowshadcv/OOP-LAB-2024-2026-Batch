7594f29ed80ae70bb022df97cec75386887d17d6
eswardas <eswardaskn@gmail.com>
2025-03-28 14:04:18 +0530

    Create EmployeeT.java

---
+++ 13_ESWARDAS_K_NAIR/11.TeacherDetails/EmployeeT.java
@@ -0,0 +1,18 @@
+package MODULE_3;
+
+public class EmployeeT 
+{
+	int empid;
+	String name;
+	float salary;
+	String address;
+	
+	EmployeeT(int empid,String name,float salary,String address)
+	{
+		this.empid = empid;
+		this.name = name;
+		this.salary = salary;
+		this.address = address;
+	}
+}
+
