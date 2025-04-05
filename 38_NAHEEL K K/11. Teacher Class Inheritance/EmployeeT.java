e042869336b18a73699a08ba9c6629070785dc18
Naheel <96335640+naheelkk@users.noreply.github.com>
2025-04-05 14:45:25 +0530

    Add files via upload

---
+++ 38_NAHEEL K K/11. Teacher Class Inheritance/EmployeeT.java
@@ -0,0 +1,14 @@
+package module_3;
+
+public class EmployeeT {
+	int empid;
+	String name,address;
+	float salary;
+	public EmployeeT(int empid, String name, String address, float salary) {
+		this.empid = empid;
+		this.name = name;
+		this.address = address;
+		this.salary = salary;
+	}
+}
+
