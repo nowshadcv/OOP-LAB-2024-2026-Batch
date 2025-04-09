2d002b93b76d67bdffcacdc1e61e63f469c7cd14
Meghna2004p <meghnamohanan2004@gmail.com>
2025-04-09 15:48:17 +0530

    Add files via upload

---
+++ 28_MEGHNA P/12. Employee Class Inheritance/EmployeeT.java
@@ -0,0 +1,16 @@
+package MODULE2;
+import java.util.Scanner;
+class EmployeeT {
+	int empid;
+	String name;
+	float salary;
+	String address;
+	EmployeeT(){}
+	EmployeeT(int empid,String name,float salary,String address){
+		this.empid=empid;
+		this.name=name;
+		this.salary=salary;
+		this.address=address;}
+
+	}
+	
