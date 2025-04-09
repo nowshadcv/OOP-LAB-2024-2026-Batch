b801869563f041e61a16ea77504fbdd0c5b3d42c
akhilapottekkatt <143066829+akhilapottekkatt@users.noreply.github.com>
2025-04-09 16:27:05 +0530

    Create EmployeeT.java

---
+++ 03_AKHILA P/12.Employee Class Inheritance/EmployeeT.java
@@ -0,0 +1,20 @@
+ package MODULE2;
+ import java.util.Scanner;
+
+public class Employeet {
+	int empid;
+	String name;
+	float salary;
+	String address;
+	Employeet(){}
+	Employeet(int empid,String name,float salary,String address){
+		this.empid =empid;
+		this.name=name;
+		this.salary=salary;
+		this.address= address;
+		
+		
+	}
+	
+
+}
