225e647ca7e16700955e30318c77ecb1b2001e06
NIYAS <niyaspa8342@gmail.com>
2025-04-09 16:39:25 +0530

    Add files via upload

---
+++ 30_MOHAMMED NIYAS P A/10. OverLoadDemo/OverLoadDemo.java
@@ -0,0 +1,40 @@
+package MODULE2;
+
+import java.util.Scanner;
+
+class OverLoadDemo {
+    void area(int side) {
+        System.out.println("Area of square: " + (side * side));
+    }
+
+    void area(int length, int breadth) {
+        System.out.println("Area of rectangle: " + (length * breadth));
+    }
+
+    void area(double radius) {
+        System.out.println("Area of circle: " + (Math.PI * radius * radius));
+    }
+
+    public static void main(String[] args) {
+        OverLoadDemo ob = new OverLoadDemo();
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Enter side of the square: ");
+        int side = scanner.nextInt();
+        ob.area(side);
+
+       
+        System.out.print("Enter length and breadth of the rectangle: ");
+        int length = scanner.nextInt();
+        int breadth = scanner.nextInt();
+        ob.area(length, breadth);
+
+        
+        System.out.print("Enter radius of the circle: ");
+        double radius = scanner.nextDouble();
+        ob.area(radius);
+
+        scanner.close();
+    }
+}
