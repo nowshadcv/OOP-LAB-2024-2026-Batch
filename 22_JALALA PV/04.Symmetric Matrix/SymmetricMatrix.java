1e56c7d15e20a7d62c35226e1eaf12a00d2702f0
jalala-pv <99414691+jalala-pv@users.noreply.github.com>
2025-04-16 23:09:01 +0530

    Update SymmetricMatrix.java

--- 22_JALALA PV/04.Symmetric Matrix/SymmetricMatrix.java
+++ 22_JALALA PV/04.Symmetric Matrix/SymmetricMatrix.java
@@ -1,51 +1,41 @@
 package MODULE1;
 import java.util.Scanner;
-
 public class SymmetricMatrix {
-
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("enter the no of rows:");
-		int rows=sc.nextInt();
-		System.out.println("enter the no of columns:");
-		int cols=sc.nextInt();
-		int matrix[][]=new int[rows][cols];
-		System.out.println("enter the elements of the matrix:");
-		for(int i=0;i<rows;i++)
-			for(int j=0;j<cols;j++)
-				matrix[i][j]=sc.nextInt();
-		sc.close();
-		System.out.println("the given matrix:");
-		for(int i=0;i<rows;i++) {
-			for(int j=0;j<cols;j++)
-				System.out.println(matrix[i][j]+"\t");
-			System.out.println();
-		}
-		if(rows!=cols)
-		{
-			System.out.println("the given matrix is not a asquare matrix");
-		}
-		else
-		{
-			boolean symmetric=true;
-			for(int i=0;i<rows;i++) 
-				for(int j=0;j<cols;j++)
-					if(matrix[i][j]!=matrix[j][i])
-						{
-						symmetric=false;
-						break;
-						}
-			if(symmetric)
-				System.out.println("the given matrix is symmetric");
-				
-			else
-				System.out.println("the given matrix is not symmetric");
-			
-			
-			
-		}
-		
-
-	}
-	}
-
+public static void main(String[] args) {
+Scanner s = new Scanner(System.in);
+System.out.print("Enter number of rows:");
+int rows = s.nextInt();
+System.out.print("Enter number of columns:");
+int cols = s.nextInt();
+int matrix[][] = new int[rows][cols];
+System.out.println("Enter the elements of matrix:");
+for(int i = 0; i < rows; i++)
+for(int j = 0; j < cols; j++)
+matrix[i][j] = s.nextInt();
+s.close();
+System.out.println("The given matrix:");
+for(int i = 0; i < rows; i++)
+{
+for(int j = 0; j < cols; j++)
+	System.out.print(matrix[i][j]+" ");
+System.out.println();
+}
+if(rows!=cols)
+System.out.println("The given matrix is not symmetric!");
+else
+{
+boolean symmetric = true;
+for(int i = 0; i < rows; i++)
+for(int j = 0; j < cols; j++)
+if(matrix[i][j]!=matrix[j][i])
+{
+symmetric = false;
+break;
+}
+if(symmetric)
+System.out.println("The given matrix is symmetric");
+else
+System.out.println("The given matrix is not symmetric");
+}
+}
+}
