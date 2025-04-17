3a6e62fd38dcfcca314a0306b5d37a38598c4f1a
jalala-pv <99414691+jalala-pv@users.noreply.github.com>
2025-04-17 12:21:53 +0530

    Update OverLoadDemo.java

--- 22_JALALA PV/11. OverLoadDemo/OverLoadDemo.java
+++ 22_JALALA PV/11. OverLoadDemo/OverLoadDemo.java
@@ -12,27 +12,26 @@ public class OverLoadDemo {
 		System.out.println("The area of the circle is "+z+"sq units");
 	}
 
-public static class OverLoad{
-	public static void main(String args[]) 
-	{
-		Scanner sc=new Scanner(System.in);
-		System.out.print("Enter side of square: ");
-		float a=sc.nextFloat();
-		System.out.print("Enter length of rectangle: ");
-		float l=sc.nextFloat();
-		System.out.print("Enter breadth of rectangle: ");
-		float b=sc.nextFloat();
-		System.out.print("Enter radius of circle(double): ");
-		double r=sc.nextDouble();
-		sc.close();
-		OverLoadDemo ob=new OverLoadDemo();
-		ob.area(a);
-		ob.area(l,b);
-		ob.area(r);
-		
-	}
-	}
+	public static class OverLoad{
+		public static void main(String args[]) 
+		{
+			Scanner sc=new Scanner(System.in);
+			System.out.print("Enter side of square: ");
+			float a=sc.nextFloat();
+			System.out.print("Enter length of rectangle: ");
+			float l=sc.nextFloat();
+			System.out.print("Enter breadth of rectangle: ");
+			float b=sc.nextFloat();
+			System.out.print("Enter radius of circle(double): ");
+			double r=sc.nextDouble();
+			sc.close();
+			OverLoadDemo ob=new OverLoadDemo();
+			ob.area(a);
+			ob.area(l,b);
+			ob.area(r);
+			
+		}
+		}
 }
-
 
 
