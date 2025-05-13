b72c95317f63cb147fc5ca6089bc740158436da4
Jasir Hassan K <jasirhassan.894315@gmail.com>
2025-05-13 16:04:46 +0530

    Create Circle.java

---
+++ 23_JASIR HASSAN K/17. Packages /figures/Circle.java
@@ -0,0 +1,14 @@
+package figures;
+
+public class Circle implements FigureCri {
+	private double radius;
+	public Circle(double r) {
+		radius = r;
+	}
+	public double perimeter() {
+		return (2*3.14*radius);
+	}
+	public double area() {
+		return (3.14*radius*radius);
+	}
+}
