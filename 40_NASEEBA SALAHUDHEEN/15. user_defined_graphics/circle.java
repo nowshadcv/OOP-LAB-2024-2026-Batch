4e1a36cdecc4db3f6caca57c89386328e6d9f646
naseeba-salah <naseebasalah44@gmail.com>
2025-05-13 16:22:11 +0530

    Create circle.java

---
+++ 40_NASEEBA SALAHUDHEEN/15. user_defined_graphics/circle.java
@@ -0,0 +1,7 @@
+package figures;
+
+public class circle implements Figureci{
+private double radius;
+public circle(double r){ radius = r;}
+public double perimeter(){return (2*3.14*radius);}
+public double area(){ return (3.14*radius*radius);}}
