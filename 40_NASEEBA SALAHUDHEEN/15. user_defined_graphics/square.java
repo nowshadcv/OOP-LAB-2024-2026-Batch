a4a943e17fd2e4d1f553ec78a21f86d899c368d1
naseeba-salah <naseebasalah44@gmail.com>
2025-05-13 16:23:10 +0530

    Create square.java

---
+++ 40_NASEEBA SALAHUDHEEN/15. user_defined_graphics/square.java
@@ -0,0 +1,10 @@
+package figures;
+
+public class square implements Figuresq{
+	
+	private int side;
+	public square(int s) {side=s;}
+	public int perimeter() {return(4*side);}
+	public int area() { return(side*side);}
+
+}
