f6755e96660134821cdacd5f48d290958986f524
Nafeesa0 <148351447+Nafeesa0@users.noreply.github.com>
2025-05-15 14:50:22 +0530

    Add files via upload

---
+++ 37_NAFEESA K T/15. InheritanceBook/InheritanceBook.java
@@ -0,0 +1,14 @@
+package MODULE_3;
+
+public class InheritanceBook {
+
+	public InheritanceBook() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
