8e146f9e6b310dc6cad336ff2b2467fa56f258a4
akhilapottekkatt <143066829+akhilapottekkatt@users.noreply.github.com>
2025-05-15 13:58:52 +0530

    Create Result.java

---
+++ 03_AKHILA P/16.Sports/Result.java
@@ -0,0 +1,7 @@
+public class Result implements Student, Sports{ 
+	public void displayScore ( ) {
+	System.out.println ("Academic Score is : "+ Student. score) ;}
+	public void displaySportsScore ( ) {
+	System.out.println ( "Sports Score is : " + Sports . score) ;}
+	
+}
