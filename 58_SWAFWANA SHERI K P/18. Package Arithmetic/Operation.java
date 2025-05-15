d23d4e98e0bff530bfa63fdaf1e18e5eea24dd60
swafwana <swafwanasheri17@gmail.com>
2025-05-15 15:40:33 +0530

    Add files via upload

---
+++ 58_SWAFWANA SHERI K P/18. Package Arithmetic/Operation.java
@@ -0,0 +1,7 @@
+package Arithmetic;
+
+public interface Operation {
+	double calculate(double a, double b);
+	
+
+}
---
+++ 58_SWAFWANA SHERI K P/18. Package Arithmetic/Substraction.java
@@ -0,0 +1,9 @@
+package Arithmetic;
+
+public class Substraction implements Operation 
+{
+	public double calculate(double a, double b) 
+	{
+		  return a - b;
+	}
+}
