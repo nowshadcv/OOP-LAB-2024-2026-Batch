ffe0fcc072dd6208b0115bb376885a6b9388c485
rosh4976 <roshnashirinm2002@gmail.com>
2025-05-15 15:42:38 +0530

    Create Division.java

---
+++ 46_ROSHNA SHIRIN M/18.arithmetic/Division.java
@@ -0,0 +1,11 @@
+package Arithmetic;
+
+
+public class Division implements Operation {
+    public double operate(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero is not allowed.");
+        }
+        return a / b;
+    }
+}
