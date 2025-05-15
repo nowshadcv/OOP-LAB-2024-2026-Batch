5b53113a8b40469434d8104455604351b649601d
Sneha <115629125+Snehasathyaa@users.noreply.github.com>
2025-05-15 14:35:05 +0530

    Create Result.java

---
+++ 41_NAYANA U/17.ImplementInterface/Result.java
@@ -0,0 +1,9 @@
+package module;
+
+public class Result implements Student,Sports {
+	public void displayScore() {
+		System.out.println("Academic score is:"+Student.score);}
+	public void displaySportsScore() {
+		System.out.println("Sports Score is :"+Sports.score);}}
+
+	
