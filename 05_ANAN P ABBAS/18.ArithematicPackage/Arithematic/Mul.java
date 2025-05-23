64775e5415e12e5a636350bb28727dacfe63139c
naseeba-salah <naseebasalah44@gmail.com>
2025-05-23 11:23:45 +0530

    Add files via upload

--- 05_ANAN P ABBAS/18.ArithematicPackage/Arithematic/Mul.java
+++ 05_ANAN P ABBAS/18.ArithematicPackage/Arithematic/Mul.java
@@ -1,17 +1,17 @@
 package Arithmetic;
 
-public class Add implements OprIF
+public class Mul implements OprIF
 {
 	private int num1,num2;
 	
-	public Add(int num1,int num2)
+	public Mul(int num1,int num2)
 	{
 		this.num1 = num1;
 		this.num2 = num2;
 	}
 	public int opr()
 	{
-		return num1+num2;
+		return num1*num2;
 	}
 	
 
