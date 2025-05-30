0a1ee267779bdc5328a506866a641b03e77bdc04
jyothishmohan <156563567+jyothishmohan@users.noreply.github.com>
2025-05-30 14:46:49 +0530

    Update EmpArrObjects.java

--- 25_JYOTHISH MOHAN A/09. Employee Details/EmpArrObjects.java
+++ 25_JYOTHISH MOHAN A/09. Employee Details/EmpArrObjects.java
@@ -1,54 +1,70 @@
-package MODULE1;
+package module1;
 import java.util.Scanner;
 
-public class EmpArrObjects {
+class Employee {
+    int id, salary;
+    String name;
 
-	public static void main(String[] args) {
-		System.out.println("Enter number of Employees : ");
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		System.out.println("Enter Employee details one by one..");
-		Employee employees[]=new Employee[n];
-		Scanner scemp=new Scanner(System.in);
-		int eid,esal;
-		String enam;
-		for(int i=0;i<n; i++) {
-			System.out.println("Enter "+i+"Employee details");
-			System.out.println("Enter employee id (integer) : ");
-			eid=scemp.nextInt();
-			scemp.nextLine();
-			System.out.println("Enter Employee Name (String ) : ");
-			String nam=scemp.nextLine();
-			enam=new String(nam);
-			System.out.println("Enter employee salary (integer) : ");
-			esal= scemp.nextInt();
-			Employee emp=new Employee(eid,esal,enam);
-			employees[i]=emp;}
-		System.out.println("Employees are : \n");
-		for(Employee y : employees)
-			y.showData();
-		System.out.println("Enter empoyee number to search :");
-		int semp=sc.nextInt();
-		boolean found = false;
-		for(Employee e : employees) {
-			if(semp==e.eNo) {
-				found=true;
-				System.out.println("Employee found");
-				e.showData();
-				break;}}
-		if(! found)
-			System.out.println("Employee not found");}
-			}
-			
-class Employee{
-	int eNo,eSalary;
-	String eName;
-	public Employee( ) {}
-	public Employee(int no,int sal,String name) {
-		eNo=no;
-		eSalary=sal;
-		eName=name;}
-	public void showData( ) {
-		System.out.print("EmpId = " + eNo +" "+"Name = " + eName+" " + "Salary = " + eSalary );
-		System.out.println();}
-	}	
+    public Employee(int id, int salary, String name) {
+        this.id = id;
+        this.salary = salary;
+        this.name = name;
+    }
+
+    public void showData() {
+        System.out.println("EmpId = " + id + " Name = " + name + " Salary = " + salary);
+    }
+}
+
+public class EmpArrObjects {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of Employees: ");
+        int n = sc.nextInt();
+        sc.nextLine(); // Consume newline
+
+        Employee[] employees = new Employee[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("\nEnter details for Employee " + (i + 1));
+
+            System.out.print("ID: ");
+            int id = sc.nextInt();
+            sc.nextLine(); // Consume newline
+
+            System.out.print("Name: ");
+            String name = sc.nextLine();
+
+            System.out.print("Salary: ");
+            int salary = sc.nextInt();
+            sc.nextLine(); // Consume newline
+
+            employees[i] = new Employee(id, salary, name);
+        }
+
+        System.out.println("\nAll Employees:");
+        for (Employee e : employees) {
+            e.showData();
+        }
+
+        System.out.print("\nEnter employee ID to search: ");
+        int searchId = sc.nextInt();
+
+        boolean found = false;
+        for (Employee e : employees) {
+            if (e.id == searchId) {
+                System.out.println("Employee found:");
+                e.showData();
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Employee not found.");
+        }
+
+        sc.close();
+    }
+}
